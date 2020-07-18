6bc848cef134e8e018b8df7eae9184bbaee32da1
Arturo J. Pérez <arturjosep@gmail.com>
2020-07-18 20:19:17 +0200

    Time planning for the scheduled tasks

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,20 @@
 extern crate panic_halt;
 
 use rtic::app;
-use rtic::cyccnt::U32Ext;
+use rtic::cyccnt::{Duration, U32Ext};
 use rtt_target::{rprintln, rtt_init_print};
 
 use crate::blinker::Blinker;
 use f3::hal::gpio::GpioExt;
+use f3::hal::prelude::*;
 use f3::hal::rcc::RccExt;
+use f3::hal::time;
+use f3::hal::time::{Hertz, MegaHertz};
 use f3::led::Leds;
 
 mod blinker;
+
+static FREQUENCY: MegaHertz = time::MegaHertz(36);
 
 #[app(device = f3::hal::stm32f30x, peripherals = true, monotonic = rtic::cyccnt::CYCCNT)]
 const APP: () = {
@@ -33,6 +38,8 @@ const APP: () = {
 
         let device = cx.device;
         let mut rcc = device.RCC.constrain();
+        let mut flash = device.FLASH.constrain();
+        rcc.cfgr.sysclk(FREQUENCY).freeze(&mut flash.acr);
 
         let led = Leds::new(device.GPIOE.split(&mut rcc.ahb));
         let blinker = Blinker::new(led);
@@ -53,12 +60,14 @@ const APP: () = {
 
     #[task(schedule = [blinky], resources = [blinker])]
     fn blinky(cx: blinky::Context) {
-        rprintln!("Blink");
+        static TASK_FREQUENCY: Hertz = Hertz(8);
 
         let blinker = cx.resources.blinker;
         blinker.run();
 
-        cx.schedule.blinky(cx.scheduled + 2_000_000.cycles()).ok();
+        cx.schedule
+            .blinky(cx.scheduled + plan_task(TASK_FREQUENCY))
+            .ok();
     }
 
     // Here we list unused interrupt vectors that can be used to dispatch software tasks
@@ -68,3 +77,8 @@ const APP: () = {
         fn CAN_RX1();
     }
 };
+
+#[inline]
+fn plan_task(frequency: Hertz) -> Duration {
+    (((1.0 / frequency.0 as f32) * (FREQUENCY.0 * 1_000_000) as f32) as u32).cycles()
+}
